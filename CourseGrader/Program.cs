22dbdd3f3dbebccccd3533f2baf85c39573fc1a1
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-04-08 23:59:28 -0500

    Stepping it out

--- CourseGrader/Program.cs
+++ CourseGrader/Program.cs
@@ -6,6 +6,10 @@ namespace CourseGrader
     {
         public static void Main(string[] args)
         {
+            int[] testScoresArray = new int[] { 70, 49, 60, 75, 92, 88, 100 };
+            Console.WriteLine(GradeTestScores(testScoresArray));
+            Console.ReadLine();
+            
             // create a method named GradeTestScores that takes a int array of test scores as its parameter and returns a string
             // GradeTestScores will add up these test scores and calculate an average score. 
             // It should then return a string of "pass" or "fail" depending on these two conditions:
@@ -13,14 +17,32 @@ namespace CourseGrader
             // If the average score is lower than 70 or at least one test score is below 50, then return a message of "fail".
         }
 
-        public static string GradedTestScores(int[] testScores)
+        public static int[] GradeTestScores(int[] testScoresArray)
         {
+            string result = null;
 
-            string result = "fail";
+            foreach (int score in testScoresArray)
+            {
 
+                int sum = 0;
+                sum += score;
+                int avg = sum / testScoresArray.Length;
+
+                if (score < 50)
+                {
+                    return result = "fail";
+
+                }
+            }
 
             return result;
+  
+
         }
+
+
+            
+       
 
     }
 }
