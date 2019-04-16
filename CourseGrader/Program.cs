171c7095a868cd10e3d66a94ae0aa6f3d3fd45ba
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-04-15 23:17:09 -0500

    Another go.

--- CourseGrader/Program.cs
+++ CourseGrader/Program.cs
@@ -3,22 +3,15 @@ namespace CourseGrader
 {
     public static class Program
     {
-        
+
 
         public static void Main(string[] args)
         {
-            int[] testScoresArray = new int[] {  };
+            int[] testScoresArray = new int[] { 0, 0, 0 };
 
             Console.WriteLine(GradeTestScores(testScoresArray));
             Console.ReadLine();
-
-
-            // create a method named GradeTestScores that takes a int array of test scores as its parameter and returns a string
-            // GradeTestScores will add up these test scores and calculate an average score. 
-            // It should then return a string of "pass" or "fail" depending on these two conditions:
-            // If the average score is greater than or equal to 70 and no single test score is below 50, then return a message of "pass".
-            // If the average score is lower than 70 or at least one test score is below 50, then return a message of "fail".
-        }
+        }   
 
         public static string GradeTestScores(int[] testScoresArray)
         {
@@ -39,25 +32,27 @@ namespace CourseGrader
                 if (avg >= 70 && minScore >= 50)
                 {
                     result = "pass";
+                    return result;
                 }
-
+                
                 if (avg < 70 || minScore < 50)
                 {
                     result = "fail";
+                    return result;
                 }
 
                 else if (testScoresArray.Length == 0)
                 {
-                    break;
+                    break;   
                 }
-                continue;
+                    continue;    
             }
 
             if (testScoresArray.Length == 0)
             {
                 result = "fail";
             }
-             return result;
+                return result;
         }
 
     }  
